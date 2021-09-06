e935e1c8f47b0d2fb04f44333ddff2eb88aa0e19
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2021-09-06 08:48:17 -0500

    Accesibility hook

--- src/App.js
+++ src/App.js
@@ -27,9 +27,41 @@ function App() {
 const modalContext = React.createContext();
 
 function Modal({ children, onModalClose }) {
+  React.useEffect(() => {
+    function keyListener(e) {
+      const listener = keyListenersMap.get(e.keyCode);
+      return listener && listener(e);
+    }
+    document.addEventListener("keydown", keyListener);
+
+    return () => document.removeEventListener("keydown", keyListener);
+  });
+
+  const modalRef = React.createRef();
+  const handleTabKey = e => {
+    const focusableModalElements = modalRef.current.querySelectorAll(
+      'a[href], button, textarea, input[type="text"], input[type="radio"], input[type="checkbox"], select'
+    );
+    const firstElement = focusableModalElements[0];
+    const lastElement =
+      focusableModalElements[focusableModalElements.length - 1];
+
+    if (!e.shiftKey && document.activeElement !== firstElement) {
+      firstElement.focus();
+      return e.preventDefault();
+    }
+
+    if (e.shiftKey && document.activeElement !== lastElement) {
+      lastElement.focus();
+      e.preventDefault();
+    }
+  };
+
+  const keyListenersMap = new Map([[27, onModalClose], [9, handleTabKey]]);
+
   return ReactDOM.createPortal(
     <div className="modal-container" role="dialog" aria-modal="true">
-      <div className="modal-content">
+      <div className="modal-content" ref={modalRef}>
         <modalContext.Provider value={{ onModalClose }}>
           {children}
         </modalContext.Provider>
