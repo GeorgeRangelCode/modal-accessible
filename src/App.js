e287f86e70b2a40c22919e6b152c69531b78d0a0
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2021-09-06 08:31:44 -0500

    Add modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
 function App() {
+  const [isModalVisible, setIsModalVisible] = React.useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Parent container</h1>
+      <h3>This is just a demo container</h3>
+      <button onClick={() => setIsModalVisible(true)}>open modal</button>
+      {isModalVisible && (
+        <Modal onModalClose={() => setIsModalVisible(false)}>
+          <Modal.Header>Header</Modal.Header>
+          <Modal.Body>Body</Modal.Body>
+          <Modal.Footer>
+            <Modal.Footer.CloseBtn>Close</Modal.Footer.CloseBtn>
+          </Modal.Footer>
+        </Modal>
+      )}
     </div>
   );
 }
+
+
+const modalContext = React.createContext();
+
+function Modal({ children, onModalClose }) {
+  return ReactDOM.createPortal(
+    <div className="modal-container" role="dialog" aria-modal="true">
+      <div className="modal-content">
+        <modalContext.Provider value={{ onModalClose }}>
+          {children}
+        </modalContext.Provider>
+      </div>
+    </div>,
+    document.body
+  );
+}
+
+Modal.Header = function ModalHeader(props) {
+  const { onModalClose } = React.useContext(modalContext);
+
+  return (
+    <div className="modal-header">
+      {props.children}
+      <button className="cross-btn" title="close modal" onClick={onModalClose}>
+        ✕
+      </button>
+    </div>
+  );
+};
+
+Modal.Body = function ModalBody(props) {
+  return <div className="modal-body">{props.children}</div>;
+};
+
+Modal.Footer = function ModalFooter(props) {
+  return <div className="modal-footer">{props.children}</div>;
+};
+
+Modal.Footer.CloseBtn = function CloseBtn(props) {
+  const { onModalClose } = React.useContext(modalContext);
+  return (
+    <button
+      {...props}
+      className="close-btn"
+      title="close modal"
+      onClick={onModalClose}
+    />
+  );
+};
 
 export default App;
